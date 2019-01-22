5934cf57ad1c2476b25a54f56715051f4302cb8e
Joshua Pollak <jpollak@zynga.com>
2019-01-22 14:43:28 -0800

    stable version of runtime output

--- test/reportGenerator.spec.js
+++ test/reportGenerator.spec.js
@@ -1,6 +1,10 @@
 const mockdata = require('./mockdata');
 const ReportGenerator = require('../src/reportGenerator');
 
+function listify(html) {
+	return html.toString().replace(/></g, '>\n<').split('\n');
+}
+
 describe('reportGenerator', () => {
 	describe('renderHtmlReport', () => {
 		let mockedConfig;
@@ -21,7 +25,7 @@ describe('reportGenerator', () => {
 			};
 		});
 
-		it('should return a HTML report based on the given input data', () => {
+		it('should return a HTML report based on the given input data', (done) => {
 			const reportGenerator = new ReportGenerator(mockedConfig);
 
 			return reportGenerator.renderHtmlReport({ data: mockdata.jestResponse.multipleTestResults, stylesheet: '' })
@@ -29,28 +33,34 @@ describe('reportGenerator', () => {
 					expect(xmlBuilderOutput).not.toBeNull();
 					expect(xmlBuilderOutput.toString()).toMatch('<html>');
 					expect(xmlBuilderOutput.toString()).toMatch('<div id="timestamp">');
-				});
+					done();
+				})
+				.catch(done);
 		});
 
-		it('should generate identical output when called twice', () => {
+		it('should generate identical output when called twice', (done) => {
 			mockedConfig.getStable = () => true;
 			const mockedData = (n) => {
 				const result = Object.create(mockdata.jestResponse.multipleTestResults);
-				result.startTime += n;
+				result.startTime += 32768 * n;
+				result.testResults[0].perfStats.start -= n * 100;
 				return result;
 			};
 			const reportGenerators = [new ReportGenerator(mockedConfig), new ReportGenerator(mockedConfig)];
 			const promises = reportGenerators.map((generator, i) => generator.renderHtmlReport({
-				data: mockedData(32768 * i),
+				data: mockedData(i),
 				stylesheet: '',
 			}));
-			Promise.all(promises).then((xmlBuilderOutputs) => {
-				const xmlBuilderOutput = xmlBuilderOutputs.pop().toString();
-				expect(xmlBuilderOutput).toMatch('<html>');
-				xmlBuilderOutputs.forEach((otherOutput) => {
-					expect(otherOutput.toString()).toEqual(xmlBuilderOutput);
-				});
-			});
+			Promise.all(promises)
+				.then((xmlBuilderOutputs) => {
+					const xmlBuilderOutput = xmlBuilderOutputs.pop().toString();
+					expect(xmlBuilderOutput).toMatch('<html>');
+					xmlBuilderOutputs.forEach((otherOutput) => {
+						expect(listify(otherOutput)).toEqual(listify(xmlBuilderOutput));
+					});
+					done();
+				})
+				.catch(done);
 		});
 
 		it('should return reject the promise if no data was provided', () => {
